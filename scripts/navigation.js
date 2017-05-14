fcc4b74f048895e57b14de3a0776901c0b9717c9
Lucas May Petry <lucasmaypetry@gmail.com>
2017-05-14 17:12:53 -0300

    Structure new webpage

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,14 +1,17 @@
 var mainDiv = null;
 
-function changeContent(newContentId) {
+function init() {
+  mainDiv = document.getElementById('main-content');
+}
+
+function changeContent(sender, content) {
+  document.getElementsByClassName("menu-selected")[0].setAttribute("class", "");
+  sender.setAttribute("class", "menu-selected");
+  
   mainDiv.style.opacity = 0;
   
   setTimeout(function() { 
-    mainDiv.innerHTML = document.getElementById(newContentId).innerHTML;
+    mainDiv.innerHTML = document.getElementById(content).innerHTML;
     mainDiv.style.opacity = 1;
   }, 400);
-}
-
-function init() {
-  mainDiv = document.getElementById('main');
 }
