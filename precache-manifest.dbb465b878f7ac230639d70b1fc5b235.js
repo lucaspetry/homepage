c12227fde3665c6a015812d9ea89726e47a7344f
Lucas May Petry <lucasmaypetry@gmail.com>
2020-06-03 14:57:31 -0300

    Updates

---
+++ precache-manifest.dbb465b878f7ac230639d70b1fc5b235.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7991f09e992ad6db7adf381a9c89a3ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc93a111f2810fe9f60",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "809963e17e3c18ecac8e",
+    "url": "/static/css/main.8a273c36.chunk.css"
+  },
+  {
+    "revision": "fbc93a111f2810fe9f60",
+    "url": "/static/js/2.b16322ae.chunk.js"
+  },
+  {
+    "revision": "67b2218b34fc0c291394ab1ce7aeb55f",
+    "url": "/static/js/2.b16322ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809963e17e3c18ecac8e",
+    "url": "/static/js/main.69f2474e.chunk.js"
+  },
+  {
+    "revision": "2c5b62721a08dcf69ebb",
+    "url": "/static/js/runtime-main.661514ca.js"
+  },
+  {
+    "revision": "09f0201e92b42591f3c447f7cd8704aa",
+    "url": "/static/media/icon-file-text.09f0201e.svg"
+  },
+  {
+    "revision": "d6c7705ae38e38506bf75da3873d05c7",
+    "url": "/static/media/profile.d6c7705a.jpg"
+  },
+  {
+    "revision": "c7b28949322ea0a20a9b00b8b9f8f38f",
+    "url": "/static/media/publications.c7b28949.tsv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.5ec88eae3ff00296208371fc350e2d6e.js"
+  "/precache-manifest.dbb465b878f7ac230639d70b1fc5b235.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
